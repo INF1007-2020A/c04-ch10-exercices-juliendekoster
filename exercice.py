b0782188b3afa3dfc82072edd4fd07c31e76c27f
juliendekoster <70984282+juliendekoster@users.noreply.github.com>
2020-11-09 19:50:32 -0500

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -8,18 +8,35 @@ import numpy as np
 
 # TODO: Définissez vos fonctions ici (il en manque quelques unes)
 def linear_values() -> np.ndarray:
-    return np.array([])
+    return np.linspace(-1.3, 2.5, num=64)
 
 
 def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
-    return np.array([])
+    # r = np.sqrt(x**2 + y**2)
+    # angle = np.arctan2(y, x)
+   # for c in cartesian_coordinates:
+       # r = np.sqrt(c[0]**2 + c[1]**2)
+        # angle = np.arctan2(c[1], c[0])
+    return np.array([(np.sqrt(c[0]**2 + c[1]**2), np.arctan2(c[1], c[0])) for c in cartesian_coordinates])
 
 
 def find_closest_index(values: np.ndarray, number: float) -> int:
-    return 0
+    
+    return np.abs(values - number).argmin()
 
+
+
+def vreate_plot():
+    x = np.linspace(-1, 1, num= 250)
+    y = x**2 * np.sin(1 / x**2) + x
+       
+    plt.scatter(x, y)
+    plt.show()
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
+    print(linear_values())
+    print(coordinate_conversion(np.array([(0, 0), (1, 1), (5, 8)])))
+    print(find_closest_index(np.array([0, 5, 10, 12, 8]), 10.5))
+    create_plot()
     pass
